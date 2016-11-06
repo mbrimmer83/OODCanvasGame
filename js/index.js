9b9c84b3472041ef934cf31eb8ba87175c01d561
Matthew <mbrimmer1@gmail.com>
2016-11-05 22:09:54 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
-class player {
-  const name = "Matthew";
-  var myName = "Matthew Thomas Brimmer";
+function Player(health, speed, power) {
+  this.health = health;
+  this.speed = speed;
+  this.power = power;
 }
+
+Player.prototype.move() {
+
+};
+
+Player.prototype.fire() {
+
+};
