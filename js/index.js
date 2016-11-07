2f63df587a35b439998b7033792ad9714257e21e
Matthew <mbrimmer1@gmail.com>
2016-11-06 23:11:09 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
-function Player(health, speed, power) {
-  this.health = health;
-  this.speed = speed;
-  this.power = power;
+class Player {
+  constructor(xPosition, yPosition, dirX, dirY, speed) {
+    this.xPosition = xPosition;
+    this.yPosition = yPosition;
+    this.dirX = dirX;
+    this.dirY = dirY;
+    this.speed = speed;
+    this.imageUrl = imageUrl;
+  }
+  move() {
+    console.log("Move");
+  }
+  fire() {
+    console.log("Fire");
+  }
+  playerImage() {
+    playerImage = new Image();
+    playerImage.src = this.imageUrl;
+  }
 }
 
-Player.prototype.move() {
+var hero = new Player(200, 200, 0 , 0, 2);
 
-};
-
-Player.prototype.fire() {
-
-};
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
