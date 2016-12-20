8213b6ecdfcc8cf463c3a4c38f9392b731fee518
Matthew <mbrimmer1@gmail.com>
2016-12-19 23:06:43 -0500

    rotation update

--- js/rotationClass.js
+++ js/rotationClass.js
@@ -2,9 +2,18 @@
 
 class RotateObject {
   constructor(object){
-
+    this.message = object.message;
   }
   imageRotation(position) {
 
   }
+
+  setMessage(string) {
+    this.message = string;
+  }
+
+  printMessage() {
+    console.log(message);
+  }
 }
+module.exports = new RotateObject({message: 'Hello from the rotation class!'})
